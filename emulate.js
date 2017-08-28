f8ce962d43cb3aa33aa0e9360b3a05a2fb762c8d
Joey Xu <is_january@sina.com>
2017-08-28 22:36:06 +0800

    enable get request with mocha

---
+++ emulate.js
@@ -0,0 +1,21 @@
+var http = require('http')
+var options = {
+  host: 'localhost',
+  port: 3000,
+  path: '/'
+}
+
+var request = http.request(options, function (resp) {
+  // console.log(res.res)
+  console.log(resp.res)
+
+  resp.on('data', (chunk) => {
+    console.log(`BODY: ${chunk}`)
+  })
+
+  resp.on('end', () => {
+    console.log('No more data in response.')
+  })
+})
+
+request.end()
--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ const Request = require('../lib/request')
 const assert = require('assert')
 const express = require('express')
 
+const app = express()
+
 describe('Request', function () {
   describe('constructor', function () {
     it('可以设置 url 和 options', function () {
@@ -43,25 +45,26 @@ describe('Request', function () {
         assert.throws(req2.get, Error, 'Error thronw')
       })
 
-      describe('可以发送请求', function () {
-        // before(function (done) {
-        //   const app = express()
-        //   app.get('/', function (req, res, next) {
-        //     console.log('BEFORE req.header')
-        //     res.send('I am ok')
-        //     next()
-        //   })
+      describe('hookd', function () {
+        let server
+        before(function () {
+          server = app.get('/', function (req, res) {
+            console.log('BEFORE req.header')
+            res.send('I am ok')
+          })
 
-        //   app.listen(3000)
-        // })
+          app.listen(3000)
+        })
 
-        it('retcode 为 0，正常取得数据', function (done) {
-          const req = new Request('http://127.0.0.1:3000')
-
-          req.get().then(res => {
-            console.log('asdfadsfsdfaf')
-            console.log(res)
-            done()
+        describe('可以发送请求', function (done) {
+          it('retcode 为 0，正常取得数据', function (done) {
+            const req = new Request('http://localhost')
+  
+            req.get().then(res => {
+              console.log('asdfadsfsdfaf')
+              console.log(res)
+              done()
+            })
           })
         })
       })
