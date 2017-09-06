5d5aa3e2ca00d150550043a680cff091442bdcbf
zh <zh@zhdeMacBook-Pro.local>
2017-09-06 17:55:46 +0800

    add test

--- demo/defer.js
+++ demo/defer.js
@@ -1,33 +1,74 @@
-
+const storage = window.localStorage;
 class Defer {
     constructor() {
         this.successCbs = [];
         this.errorCbs = [];
         this.catchCbs = [];
+        this.params = {};
     }
-    fire(cbs, params) {
+    fire(cbs, params, flag) {
+
         cbs.forEach((fn) => {
-            console.log(fn)
-            fn.call(this, params);
+            try {
+                fn.call(this, params, flag);
+            } catch (err) {
+                this.fire(this.catchCbs, err);
+            }
         })
     }
-    get() {
+    get(params) {
+        this.params = params;
+        // storage.setItem('res','')
         return this;
     }
     done(fn) {
         this.successCbs.push(fn);
+        var storageData = storage.getItem(this.params.url);
+        if(this.params.maxAge){
+            var storageDataObj = JSON.parse(storageData);
+        }
+        if (storageData) {
+            this.resolve(JSON.parse(storageData).res, true);
+        }
+        this.sendAjax();
         return this;
     }
     fail(fn) {
         this.errorCbs.push(fn);
         return this;
     }
-    resolve(params) {
-        this.fire(this.successCbs, params);
+    resolve(params, flag) {
+        this.fire(this.successCbs, params, flag);
         return this;
     }
-    reject(params) {
-        this.fire(this.errorCbs, params);
+    reject(params, flag) {
+        this.fire(this.errorCbs, params, flag);
         return this;
+    }
+    catch (fn) {
+        this.catchCbs.push(fn);
+        return this;
+    }
+    sendAjax() {
+        var that = this;
+        $.ajax({
+            url: this.params.url,
+            method: 'GET',
+            data: this.params.data || {},
+            success: function(res) {
+                if (res.retcode == 0) {
+                    var curTime = new Date().getTime();
+                    storage.setItem(that.params.url, JSON.stringify({res:res,time:curTime}));
+                    if(!that.params.lazy){
+                        that.resolve(res, false);
+                    }
+                } else {
+                    that.reject(res);
+                }
+            },
+            error: function(err) {
+                that.fire(that.catchCbs, err);
+            }
+        })
     }
 }
--- test/demo.test.js
+++ test/demo.test.js
@@ -1,11 +1,118 @@
-describe('调用请求',function(){
-    it('测试调用get',function(done){
-        var def = new Defer;
-        def.done(function(e){
-            done();
-        })
-        setTimeout(function(){
-            def.resolve(1);
-        },200)
+$.mockjax({
+    url: "/data/get",
+    responseText: {
+        retcode: 0,
+        msg: "成功",
+        res: {
+            a: 1
+        }
+    }
+});
+// describe('测试链式调用', function() {
+//     it('测试调用get', function(done) {
+//         var def = new Defer;
+//         def.done(function(res) {
+//             done();
+//         }).fail(function(res) {
+//             done();
+//         }).catch(function(e) {})
+//         setTimeout(function() {
+//             def.reject(1);
+//         }, 200)
+
+//     })
+// });
+
+// describe('两次', function() {
+//     it('两次调用done', function(done) {
+//         var def = new Defer;
+//         let n = 0;
+//         def.done(function(res) {
+//             n++;
+//             if (n === 2) done();
+//         })
+//         setTimeout(function() {
+//             def.resolve(1);
+//             def.resolve(2)
+//         }, 200);
+//     });
+
+// });
+
+// describe('捕获错误', function() {
+//     it('全部不抛出错误，打印错误', function(done) {
+//         var def = new Defer;
+//         def.done(function(res) {
+//             lasjdlfa;
+//             done();
+//         }).catch(function(e) {
+//             done();
+//         })
+//         setTimeout(function() {
+//             def.resolve(1)
+//         }, 200);
+//     })
+// });
+// describe('测试get获取数据', function() {
+//     it('测试各个回调是否完善', function(done) {
+//         let def = new Defer;
+//         let n = 0;
+//         def.get({
+//             url: '/data/get',
+//             data: { a: 1 }
+//         }).done(function(res, flag) {
+//             // console.log(res)
+//             // console.log(flag)
+//             done();
+//         }).fail(function(res) {
+//             console.log(res);
+//             done();
+//         }).catch(function(err) {
+//             console.error(err)
+//             done()
+//         });
+//     });
+//     it('有缓存调用两次', function(done) {
+//         let def = new Defer;
+//         let n = 0;
+
+//         def.get({
+//             url: '/data/get',
+//             data: { a: 1 }
+//         }).done(function(res, flag) {
+//             n++;
+//             if (n === 2) done();
+//         })
+
+//     });
+// });
+
+//正常情况就是调用两次
+describe('懒更新测试', function() {
+    it('lazy为ture时立即返回', function(done) {
+        /**
+         * data 参数是请求入参
+         * lazy 标记懒更新
+         */
+        let def = new Defer;
+        let n = 1;
+        def.get({
+                url: "/data/get",
+                data: { uin: 1234 },
+                lazy: true
+            })
+            // 所以有缓存的时候，res应当立刻返回
+            // 所以，res应当是上次的数据
+            .done(function(res, flag) {
+                // should.strictEqual({
+                //     retcode: 0,
+                //     msg: "成功",
+                //     res: {
+                //         a: 1
+                //     }
+                // }, res, '[message]')
+                n--;
+                if (n === 0) done()
+            })
     })
 })
