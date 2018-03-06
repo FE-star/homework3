7f99329b08c57b992e6a55b4e34aa7cde51c2f2f
胡泽涵 <278669145@qq.com>
2018-03-06 18:47:18 +0800

    try fix

--- test/request.js
+++ test/request.js
@@ -9,33 +9,33 @@ describe('FE-Request', function () {
   const R = request("http://127.0.0.1:3011");
   const F = request("http://127.0.0.1:3011/test_fail");
   const C = request("xxx");
-  it('可以正常请求接口', function (finish) {
+  it('可以正常请求接口', function (done) {
     R.get({ maxAge: 10000 }).done((res, flag) => { 
       console.log(res, flag);
-      finish();
+      done();
     })
   })
-  it('失败了应该告诉失败原因', function (finish) {
+  it('失败了应该告诉失败原因', function (done) {
     F.get().done(res => {
     }).fail(res => {
       console.log(res);
-      finish();
+      done();
     })
   })
-  it('出错了应当能捕捉错误', function (finish) {
+  it('出错了应当能捕捉错误', function (done) {
     C.get().done(res => { }).fail(res => { }).catch(e => {
       console.log("成功捕捉错误", e);
-      finish();
+      done();
     })
   })
-  it('lazy参数为true时，有缓存应该从缓存里拿数据', function (finish) {
+  it('lazy参数为true时，有缓存应该从缓存里拿数据', function (done) {
     R.get({ data: { desc: "mother fucker" }, lazy: true }).done((res, flag) => {
       if (flag == 0) {
         console.log("缓存没有东西，直接请求获取数据", flag == 0)
-        return finish();
+        return done();
       }
       console.log("lazy参数为true时，有缓存应该从缓存里拿数据", flag == 1);
-      finish(assert.equal(flag, 1));
+      done(assert.equal(flag, 1));
     })
   })
 })
